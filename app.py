0c076a808a55ae57abb2d6ee4fe4cc69096d93f1
sébastien Gonin <sebastiengonin@MacBook-Air-de-sebastien.local>
2025-04-08 12:11:49 +0200

    Ajout de l’en-tête HSTS pour forcer HTTPS

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ Session(app)
 
 @app.after_request
 def add_security_headers(response):
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["Referrer-Policy"] = "no-referrer"
